5f7a997bc6e6f21b193f6bc50d665d647d0c7842
domgoodwin <d0m182goodwin@gmail.com>
2017-07-30 15:59:12 +0100

    Adding further information from course in

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -137,8 +137,8 @@ object HelloWorld extends App{
   // Match expressions used more commonly then if/else
   var dayOfWeek = "Tuesday";
   val dayInFrench = dayOfWeek match{ // variable then declaration match
-      case "Monday" => "Lundi" // expressions so these are returned if match
-      case "Tuesday" => "Mardi" // Return value is on right of arrow symbol
+      case "Monday" => "Lundi"; // expressions so these are returned if match
+      case "Tuesday" => "Mardi"; // Return value is on right of arrow symbol
       // if no match, scala match error is raised, to prevent this use a case with catch
   }
   println(dayInFrench);
@@ -147,12 +147,137 @@ object HelloWorld extends App{
   //Pattern guards - adds an if onto a case
   val dayStart = dayOfWeek match{
       //case "Monday" => "Monday: 9AM"
-      case "Saturday"|"Sunday" => s"$dayOfWeek: 11AM" // Allows two or more values in a match using Or-ed
-      case "Tuesday"|"Wednesday"|"Thursday"|"Friday" => s"$dayOfWeek: 10AM" // compound OR condition inside a match
-      case bankHoliday if bankHoliday == "Monday" => s"$bankHoliday: 1PM" // Pattern guard starts with placeholder var called value binding
+      case "Saturday"|"Sunday" => s"$dayOfWeek: 11AM"; // Allows two or more values in a match using Or-ed
+      case "Tuesday"|"Wednesday"|"Thursday"|"Friday" => s"$dayOfWeek: 10AM"; // compound OR condition inside a match
+      case bankHoliday if bankHoliday == "Monday" => s"$bankHoliday: 1PM"; // Pattern guard starts with placeholder var called value binding
       // assigns match var and then if uses arrow decimeter
   }
   println(dayStart);
+
+  // catch-all/match-all within a match expression
+  val invalidMatch = "Sanday";
+  val dayMatch = invalidMatch match{
+      case "Monday" => "Monday match!";
+      // To prevent scala match error
+      // _ Is an unnamed wildcard for inputput valid, it will match anything, must be used on the left side of =>
+     case _ => { // Wilcard operator patterns
+        println(s"Error! Invalid say $invalidMatch"); // Does not bind the match value to any other values
+        invalidMatch; // expression will return this, underscore means any value
+      };
+     case bankHoliday => { // Value binding patters
+        println(s"Hit a bank holiday: $bankHoliday");
+        bankHoliday; // This expression will be hit for any value and just return it
+      };
+  }
+  // Underscore is a match all so used in a match it ends up catching all
+
+  // Match expressionss - down casting with pattern variables
+  // Unluke java, scala can use match to test type
+  val radi:Any = 10.10; // Inference will pick what type it thinks it is
+  // If we marked above as String it would through an error checking the :Int
+  val testType = radi match{
+     case radi:Int => "Integer";
+     case radi:String => "String";
+     case _:AnyRef => "Some other reference type";// Placehold _ can be used to match type
+     case _ => "Any"; // you have to have a match-all catch-all to cover all scenarios (like a default)
+  }
+  println("Type is: " + testType);
+  // Scruntinee (variable whos type is matched) must be a base type
+  // This is so you use when wanting to perform a downcast, this is why you must use Any
+
+  //=====For loops=====
+  // For loops can be expressions(returns) OR statements(doesnt return)
+  // For loops with yield are expressions so that is yields a collection of return values of each iteration
+  val dayOfWeekList = List("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun");
+  println(dayOfWeekList); // This will be a List[String] - same as List<String> in Java
+  for(day <- dayOfWeekList){ // Similar to ForEach in Java, will loop through each item in list assigning to day variable
+    day match{
+      case "Mon" => println("Monday");
+      case _ => print(day + ", ");
+    }
+  } // Nothing is returned from this for loop so it is a statement (no yield)
+  println("-This is a pretty normal java-esque for loop");
+  // Vaue binding for loop
+  val looper = for(day <- dayOfWeekList) yield { // yield here will make it return values into a collection
+    day match{
+      case "Mon" => "Manic Monday";
+      case otherDay => otherDay;
+    }
+  }
+  println("This has been returned from the for loop: " + looper);
+  // This means you dont have to collect results of loop into list with (*.add(item)) loads of times
+  // List instead comes with you when used as a yield for
+
+  // for loop different iterator types
+  // you can loops through using index or every element of list
+  // for above uses value binding method of looping
+  val indexLooper = for( i <- 0 until dayOfWeekList.size){ // this uses index, until replaces to and means you dont need to do -1 of size
+    dayOfWeekList(i);
+  } // to goes up to and onto size value, until goes to the -1 value
+  // These functions are aren't best way, foreach, map and flatmap are better ones to use
+  // These are functions so they can be chained - still useful to know however
+
+  //for loop with an if statement using pattern guards
+  for ( day <- dayOfWeekList if day == "Mon") { // for loop with if condition too
+    println("This for if was done in one line!: " + day);
+  }
+
+  //Nested for loops
+  for{i <- 0 until 3 // notice no comma seperating the 2 ranges
+      j <- dayOfWeekList}{ // you can also combine types of iterators, wow!
+    print(s"$i, $j, ");
+  }
+  println("Nested for loop using a single for!");
+
+  //=====While loops/do-while loops=====
+  // Purely statements in scala not optional expressions like for
+  // while use dont fit with functional so they should be used for writing to DB or something as no returns
+  // while loops need mutable variables so it can terminate (var)
+
+  //=====Functions in scala - first class functions=====
+  // Partially applied and currying
+  // In scala there is a clear different between functions and methods
+  //**Methods are used to define object behaviour**
+  def max(x:Int, y:Int): Int ={ // Def to specify defining a method, method signature specifies params and return type
+    if(x>y) x else y // returns a value based on calc
+  } // Curly braces are optional if the body is a single line
+  // main is a method too (def main (args: Array[String]) ={ //main code }
+  // in order to be static scala 'fakes it and puts it inside singleton object
+
+  //**Functions/functon literals
+  // Function literal
+  (x:Int)=>x+1; // simple nameless function literal, not stored in variable or given name, but it is an object, type: function1 infered
+  // Function trait has different descended types, functionx represents a function with x arguments until function23 (max arg count)
+  // First class citizenship for functions:
+  // Functions are objects (AnyRef) and can be stored in a variable, methods can return functions and methods can take a function as a param/arg
+  // Helps for distributed computing applications
+
+  //Method example, (methods are not objects)
+  def getArea2 (radius:Double):Double ={ // Method called getArea2, takes one double param and returns a double
+    val PI = 3.14;
+    PI * radius * radius;
+  } // This would be a method of whatever class contains this definition
+  // Calling this would be className.getArea2(10) for examaple
+  // Function example
+  val getArea = (radius:Double) =>{
+    val PI = 3.14;
+    PI * radius * radius;
+  }:Double //could not do this due to type inference
+  // Functions are value types - can be stored in val and var, methods are not but can be converted to function objects
+  // Functions have higher overhead and slightly slower, methods are faster and better for performance
+  // A function is not associated with a class whereas a method is
+  // Functions are basically parametrised expression blocks
+  // A defined function would be type Double => Double = <function1>
+  // A defined method would be: (radius: Double)Double (in GRPL)
+
+  // Functions are named, reusable expression blocks in any scope
+  // Method are named and useable in a certain scope unless converted to a function then above applies
+
+
+
+
+
+
 
 
 
